06d9f9997330a5ed732436aca5e8e51404d784bb
IsabellaSchoen <schoen.isabella@gmx.net>
2021-05-27 13:55:47 +0200

    nothing...

--- src/parse/lex.rs
+++ src/parse/lex.rs
@@ -1,5 +1,6 @@
 // An iterator with a peek() that returns
 // an optional reference to the next element
+// Peekable is a single, specific, concrete type...
 use std::iter::Peekable;
 use std::str::Chars;
 
@@ -19,14 +20,16 @@ fn take_number(it: &mut Peekable<Chars>) -> f64 {
     it.peeking_take_while(|&c| ('0'..='9').contains(&c) || c == '.')
         .collect::<String>()
         .parse::<f64>()
-        .unwrap()
+        .unwrap() //  will give you the embedded T
 }
 
 pub fn tokenize(input: String) -> Vec<Token> {
     let mut tokens = vec![];
 
+    // https://stackoverflow.com/questions/26333439/how-to-use-rusts-peekable
+    // ... that is constructed by calling peekable() on an interator
     let mut it = input.chars().peekable();
-    while let Some(&next) = it.peek() {
+    while let Some(&next) = it.peek() { // peek(value)
         match next {
             '0'..='9' => tokens.push(Token::Number(take_number(&mut it))),
             '(' | ')' => tokens.push(Token::Brace(it.next().unwrap())),
@@ -36,36 +39,4 @@ pub fn tokenize(input: String) -> Vec<Token> {
     }
 
     tokens
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    macro_rules! test_tokenize {
-        ($($name:ident: $input:expr => $expected:expr),*) => {
-            $(
-                #[test]
-                fn $name() {
-                    let out = tokenize($input.to_string());
-                    assert_eq!(out, $expected);
-                }
-            )*
-        };
-    }
-
-    test_tokenize!(
-        tokenize_empty: "()" => vec![Token::Brace('('), Token::Brace(')')],
-        tokenize_integer: "1" => vec![Token::Num(1.0)],
-        tokenize_long_integer: "1234" => vec![Token::Number(1234.0)],
-        tokenize_float: "1.234" => vec![Token::Num(1.234)],
-        tokenize_integer_expr: "(1)" => vec![Token::Brace('('), Token::Number(1.0), Token::Brace(')')]
-    );
-
-    // tokenize_one: "(foo)" => vec![Token::Brace('('), Token::Atom("foo".to_string()), Token::Brace(')')]
-    // tokenize_atom: "atom" => vec![Token::Atom("atom".to_string())],
-    // tokenize_two: "(foo bar)" => vec!["(", "foo", "bar", ")"],
-    // tokenize_three: "(foo bar baz)" => vec!["(", "foo", "bar", "baz", ")"],
-    // tokenize_add: "(+ 1 2)" => vec!["(", "+", "1", "2", ")"],
-    // tokenize_nested: "(+ 1 (- 3 2))" => vec!["(", "+", "1", "(", "-", "3", "2", ")", ")"]
 }
