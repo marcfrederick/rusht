be951e14954a3ec6dec3f3f9e9532e47e4d0d8e9
IsabellaSchoen <schoen.isabella@gmx.net>
2021-05-27 15:02:15 +0200

    started with lex.rs

--- src/parse/lex.rs
+++ src/parse/lex.rs
@@ -1,42 +1,52 @@
-// An iterator with a peek() that returns
-// an optional reference to the next element
-// Peekable is a single, specific, concrete type...
+// impl TokenStream
+
 use std::iter::Peekable;
 use std::str::Chars;
 
 use peeking_take_while::PeekableExt;
 
-// PartialEq -> eq for comparing
-// Derive -> automatically creates the implementation
-// required to make this `enum` printable
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub enum Token {
     Atom(String), // Symbole für Mathematik
     Number(f64), // Zahlen
     Brace(char), // Klammern
 }
 
+pub fn math(sign: &mut Peekable<Chars>) -> f64
+{
+    let mut iter = sign.chars();
+
+    match iter
+    {
+        '+' => add(),
+        '-' => sub(),
+        '*' => mult(),
+        '/' => sub(),
+    }
+}
+
 fn take_number(it: &mut Peekable<Chars>) -> f64 {
     it.peeking_take_while(|&c| ('0'..='9').contains(&c) || c == '.')
         .collect::<String>()
         .parse::<f64>()
-        .unwrap() //  will give you the embedded T
+        .unwrap()
 }
 
-pub fn tokenize(input: String) -> Vec<Token> {
-    let mut tokens = vec![];
 
-    // https://stackoverflow.com/questions/26333439/how-to-use-rusts-peekable
-    // ... that is constructed by calling peekable() on an interator
-    let mut it = input.chars().peekable();
-    while let Some(&next) = it.peek() { // peek(value)
-        match next {
-            '0'..='9' => tokens.push(Token::Number(take_number(&mut it))),
-            '(' | ')' => tokens.push(Token::Brace(it.next().unwrap())),
-            '+' | '-' | '/' | '*' => tokens.push(Token::Atom(it.next().unwrap())),
-            _ => unimplemented!()
-        };
-    }
+pub fn tokenizer(input: &mut String) -> Vec<String>
+{
+    let tokenstream: Vec<String> = Vec::new();
 
-    tokens
+    let mut iter = input.chars().peekable();
+    while let Some(&next) = iter.peek() {
+        match it 
+        {
+            '(' | ')' => tokenstream.push(Self::Brace),
+            '0'..='9' => tokenstream.push(Self::Number(it.next().unwrap())),
+            '+' | '-' | '*' | '/' => tokenstream.push(Self::Atom(it.next().unwrap())),
+        }
 }
+
+
+// https://blog.logrocket.com/macros-in-rust-a-tutorial-with-examples/
+// https://doc.rust-lang.org/rust-by-example/macros.html
