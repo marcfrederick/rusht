4681df9ed559398e0657c15959a42148d3365375
IsabellaSchoen <schoen.isabella@gmx.net>
2021-06-08 00:57:22 +0200

    math function added to hashmap and wrote each function

--- rusht/src/interpret.rs
+++ rusht/src/interpret.rs
@@ -17,9 +17,16 @@ macro_rules! hash_map {
 }
 
 pub fn interpret(ast: Ast) -> Token {
-    let env = hash_map!(
-        "+" => prelude::add
-    );
+    let env = hash_map! {
+        "+" => prelude::add,
+        "add" => prelude::add,
+        "-" => prelude::sub,
+        "sub" => prelude::sub,
+        "*" => prelude::mul,
+        "mul" => prelude::mul,
+        "/" => prelude::div,
+        "div" => prelude::div
+    };
 
     match ast {
         Ast::Atom(token) => token,
@@ -69,5 +76,20 @@ mod test {
             ]),
         ]));
         assert_eq!(out, Token::Num(24.0))
+    }
+
+
+    #[test]
+    fn nested_math() {
+        let out = interpret(Ast::List(vec![
+            Ast::Atom(Token::Ident(String::from("*"))),
+            Ast::Atom(Token::Num(10.0)),
+            Ast::List(vec![
+                Ast::Atom(Token::Ident(String::from("/"))),
+                Ast::Atom(Token::Num(10.0)),
+                Ast::Atom(Token::Num(5.0)),
+            ]),
+        ]));
+        assert_eq!(out, Token::Num(20.0))
     }
 }
--- rusht/src/prelude.rs
+++ rusht/src/prelude.rs
@@ -1,10 +1,50 @@
 use crate::tokenize::Token;
+use std::borrow::Borrow;
 
 pub fn add(args: Vec<Token>) -> Token {
     let mut sum = 0.0;
     for x in args {
         match x {
             Token::Num(n) => sum += n,
+            _ => panic!("Not a number")
+        }
+    }
+    Token::Num(sum)
+}
+
+pub fn sub(args: Vec<Token>) -> Token {
+    let mut sum: f64 = match args[0] {
+        Token::Num(n) => n + n,
+        _ => panic!("Not a number")
+    };
+    for x in args {
+        match x {
+            Token::Num(n) => sum -= n,
+            _ => panic!("Not a number")
+        }
+    }
+    Token::Num(sum)
+}
+
+pub fn mul(args: Vec<Token>) -> Token {
+    let mut sum = 1.0;
+    for x in args {
+        match x {
+            Token::Num(n) => sum *= n,
+            _ => panic!("Not a number")
+        }
+    }
+    Token::Num(sum)
+}
+
+pub fn div(args: Vec<Token>) -> Token {
+    let mut sum: f64 = match args[0] {
+        Token::Num(n) => n * n,
+        _ => panic!("Not a number")
+    };
+    for x in args {
+        match x {
+            Token::Num(n) => sum /= n,
             _ => panic!("Not a number")
         }
     }
