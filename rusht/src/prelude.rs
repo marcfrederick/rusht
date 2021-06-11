923b36ff92af6b6fbd1461ac95e32af909bad796
Marc Troelitzsch <Marc.Troelitzsch@gmail.com>
2021-06-11 12:34:54 +0200

    prelude tests

--- rusht/src/prelude.rs
+++ rusht/src/prelude.rs
@@ -77,3 +77,31 @@ fn div(args: Vec<Token>) -> Token {
     }
     Token::Num(sum)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use super::Token::*;
+
+    macro_rules! test_fn {
+        ($($func:ident - $name:ident: $input:expr => $expected:expr),*) => {
+            $(
+                #[test]
+                fn $name() {
+                    assert_eq!($func($input), $expected);
+                }
+            )*
+        };
+    }
+
+    test_fn! {
+        add - add_two: vec![Num(1.0), Num(3.0)] => Num(4.0),
+        add - add_three: vec![Num(1.0), Num(3.0), Num(10.0)] => Num(14.0),
+        sub - sub_two: vec![Num(1.0), Num(3.0)] => Num(-2.0),
+        sub - sub_four: vec![Num(1.0), Num(3.0), Num(2.0), Num(5.0)] => Num(-9.0),
+        mul - mul_two: vec![Num(2.0), Num(3.0)] => Num(6.0),
+        mul - mul_three: vec![Num(1.0), Num(3.0), Num(10.0)] => Num(30.0),
+        div - div_two: vec![Num(3.0), Num(2.0)] => Num(1.5),
+        div - div_three: vec![Num(3.0), Num(2.0), Num(0.5)] => Num(3.0)
+    }
+}
