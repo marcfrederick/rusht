1a80aee0879c78f2498cccf6737e08a2073959fe
Marc Troelitzsch <Marc.Troelitzsch@gmail.com>
2021-06-13 20:14:31 +0200

    docs

--- rusht/src/prelude.rs
+++ rusht/src/prelude.rs
@@ -44,8 +44,10 @@ impl From<Token> for String {
     }
 }
 
+/// A key value mapping of function names and the accompanying implementation.
 pub type Prelude = HashMap<String, fn(Vec<Token>) -> Token>;
 
+/// Returns a prelude (standard library) of often used functions.
 pub fn get_prelude() -> Prelude {
     prelude!(
         "+" => reduce!(|a, b| a + b => Token::Num),
@@ -60,12 +62,34 @@ pub fn get_prelude() -> Prelude {
     )
 }
 
+/// Reduces the given vector of `Token`s  using the given `reducer` function.
+/// The result is turned back into a vector using the `finalizer` function.
+///
+/// # Arguments
+///
+/// * `args` - The arguments passed to the function.
+/// * `reducer` - A function used to reduce the args to a single value.
+/// * `finalizer` - A function used to turn the result back into a `Token`.
+///
+/// # Errors
+///
+/// If the vector of args is empty, an error type will be returned.
+///
+/// # Panics
+///
+/// If one of the args can't be converted to a matching type, a panic occurs.
 fn reduce<T, F, G>(args: Vec<Token>, reducer: F, finalizer: G) -> Result<Token>
     where
         T: From<Token>,
         F: Fn(T, T) -> T,
         G: Fn(T) -> Token
 {
+    // The `finalizer` could be replaced by adding `+ Into<Token>` to the type
+    // constraints of `T`. We have decided against this approach for the
+    // added readability.
+    // The alternative implementation would require us to specify the type
+    // encapsulated in the specific `Token` variant, so not much writing effort
+    // would be saved anyways.
     args
         .into_iter()
         .map(|x| x.into())
