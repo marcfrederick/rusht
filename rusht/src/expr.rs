852b667f003677c9115ba735180efffcc94a2d09
Marc Troelitzsch <Marc.Troelitzsch@gmail.com>
2021-06-28 13:29:29 +0200

    expr stringify

--- rusht/src/expr.rs
+++ rusht/src/expr.rs
@@ -25,14 +25,31 @@ impl Display for Expr {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Expr::Num(x) => write!(f, "{}", x),
-            Expr::Str(x) => write!(f, "{}", x),
+            Expr::Str(x) => write!(f, "\"{}\"", x),
             Expr::Ident(x) => write!(f, "{}", x),
             Expr::Bool(x) => write!(f, "{}", x),
-            Expr::Lambda { .. } => write!(f, "ƛ"),
-            Expr::List(_) => todo!(),
-            Expr::Func(_) => todo!(),
+            Expr::Lambda(Lambda { args, body }) => {
+                write!(f, "λ {} -> {}", stringify(args), body.to_string())
+            }
+            Expr::List(list) => write!(f, "{}", stringify(list)),
+            Expr::Func(_) => write!(f, "prelude function"),
         }
     }
+}
+
+/// Returns the string representation of a given slice.
+///
+/// # Arguments
+///
+/// * `xs` - The slice for which to generate a string representation.
+fn stringify<T>(xs: &[T]) -> String
+where
+    T: ToString,
+{
+    format!(
+        "({})",
+        xs.iter().map(T::to_string).collect::<Vec<_>>().join(" ")
+    )
 }
 
 impl From<f64> for Expr {
@@ -106,5 +123,35 @@ impl TryFrom<Expr> for bool {
             Expr::Str(s) if ["false", "0", ""].contains(&s.trim()) => Ok(false),
             _ => Err(Error::CouldNotCoerceType),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_stringify_string_slice() {
+        assert_eq!(stringify(&["foo", "bar"]), "(foo bar)")
+    }
+
+    #[test]
+    fn test_stringify_expr_slice() {
+        assert_eq!(
+            stringify(&[
+                Expr::Num(5.0),
+                Expr::Str("foo".to_string()),
+                Expr::List(vec![Expr::Ident("bar".to_string()), Expr::Bool(true),]),
+                Expr::Lambda(Lambda {
+                    args: vec!["a".to_string()],
+                    body: Box::from(Expr::List(vec![
+                        Expr::Ident("+".to_string()),
+                        Expr::Ident("a".to_string()),
+                        Expr::Num(1.0)
+                    ])),
+                })
+            ]),
+            "(5 \"foo\" (bar true) ƛ (a) -> (+ a 1))"
+        )
     }
 }
